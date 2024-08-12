9954b89c8ca7b03294aad69891c881e23b76a10c
brn342 <bruno.ocampo@correo.ucu.edu.uy>
2024-08-12 09:16:55 -0300

    Implementación de la divicion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,6 +6,16 @@
     }
 }
 
+
+// Esta clase implementa la operación division.
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
+    }
+}
+
 Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
